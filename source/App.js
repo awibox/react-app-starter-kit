6871297d71a5fa5f348115a03c4ae387dbee2794
Andrey Arkhipov <wibox@ya.ru>
2016-11-18 00:14:27 +0300

    Cjmponents

--- source/App.js
+++ source/App.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 
-
-class App extends Component {
-
+class App extends React.Component {
     render() {
         return (
             <div>
-                <h2>Регистрация</h2>
+                <Header/>
+                <Content/>
             </div>
         );
     }
 }
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Header</h1>
+            </div>
+        );
+    }
+}
+
+class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Content</h2>
+                <p>The content text!!!</p>
+            </div>
+        );
+    }
+}
+
 
 export default App;
