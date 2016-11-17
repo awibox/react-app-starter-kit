526f15af322e8ab2a6f59c9131275a0f3130bad1
Andrey Arkhipov <wibox@ya.ru>
2016-11-18 00:52:39 +0300

    Компоненты в своей директории

--- source/App.js
+++ source/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Header from './components/header/Header';
+import Content from './components/content/Content';
 
 class App extends React.Component {
     render() {
@@ -10,27 +12,5 @@ class App extends React.Component {
         );
     }
 }
-
-class Header extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Header</h1>
-            </div>
-        );
-    }
-}
-
-class Content extends React.Component {
-    render() {
-        return (
-            <div>
-                <h2>Content</h2>
-                <p>The content text!!!</p>
-            </div>
-        );
-    }
-}
-
 
 export default App;
