b22b3fd211ec3d3a839637ad7b62c7bd0d5dfb67
Andrey Arkhipov <wibox@ya.ru>
2016-11-24 00:26:01 +0300

    require es6-promise polyfill

--- source/main.js
+++ source/main.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import Router from './router';
 import Store from './store';
+require('es6-promise').polyfill();
 
 ReactDOM.render(
     <Provider store={Store}>{Router}</Provider>,
