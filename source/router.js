a9c73de467b431696a7b3a8990c3fa9e918125db
Andrey Arkhipov <wibox@ya.ru>
2016-11-25 00:52:26 +0300

    New routings

--- source/router.js
+++ source/router.js
@@ -6,16 +6,13 @@ import MainLayout from './layouts/main/main';
 
 // Pages
 import Home from './components/home/home';
-import About from './components/about/about';
-import Github from './components/github/github';
+import Repos from './components/repos/repos';
 
 export default (
     <Router history={browserHistory}>
         <Route component={MainLayout}>
             <Route path="/" component={Home} />
-            <Route path="/about" component={About}>
-                <IndexRoute component={Github} />
-            </Route>
+            <Route path="repos" component={Repos} />
         </Route>
     </Router>
 );
