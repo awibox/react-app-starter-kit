98bb573604e892afdf8b87a9452ebc3051ae2f13
Andrey Arkhipov <wibox@ya.ru>
2016-11-25 00:51:35 +0300

    Add views

---
+++ source/views/repos-list/repos-list.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+// Using "Stateless Functional Components"
+export default function(props) {
+    console.log('repos', props);
+    return (
+        <div className="data-list">
+
+            {props.users.map(user => {
+
+                return (
+                    <div key={user.id} className="data-list-item">
+                        <div className="details">
+                            {user.name}
+                        </div>
+                    </div>
+                );
+
+            })}
+
+        </div>
+    );
+}
