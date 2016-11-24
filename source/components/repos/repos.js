751e41bb268df28a0edd931748901ba6f2fedf37
Andrey Arkhipov <wibox@ya.ru>
2016-11-25 00:51:11 +0300

    New component repos

---
+++ source/components/repos/repos.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import ReposList from '../../views/repos-list/repos-list';
+import * as reposApi from '../../api/repos-api';
+import store from '../../store';
+
+const ReposListContainer = React.createClass({
+
+    componentDidMount: function() {
+        reposApi.getRepos();
+    },
+
+    render: function() {
+        return (
+            <ReposList users={this.props.users} />
+        );
+    }
+
+});
+
+const mapStateToProps = function(store) {
+    return {
+        users: store.reposState.repos
+    };
+};
+
+export default connect(mapStateToProps)(ReposListContainer);
