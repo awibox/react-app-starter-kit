9f9739ff2b673178bff2725c3ecc36eb82421015
Andrey Arkhipov <wibox@ya.ru>
2016-11-29 01:04:03 +0300

    Upgrade API from home compoment

--- source/api/home-api.js
+++ source/api/home-api.js
@@ -4,7 +4,7 @@ import { getHomeSuccess} from '../actions/home-actions';
 
 export function getHome() {
     console.log('getHome сработал');
-    return axios.get('https://api.github.com/users/BEM-builder')
+    return axios.get('https://api.github.com/repos/BEM-builder/react-app-starter-kit')
         .then(response => {
             console.log(response);
             store.dispatch(getHomeSuccess(response.data));
--- source/views/home-info/home-info.js
+++ source/views/home-info/home-info.js
@@ -4,17 +4,23 @@ import './home-info.scss';
 
 // Using "Stateless Functional Components"
 export default function(props) {
+    let avatar_url;
+    for(let data in props.home.owner) {
+        if(data == 'avatar_url') {
+            avatar_url = props.home.owner[data];
+        }
+    }
     const companyImage = {
-        backgroundImage: 'url(' + props.home.avatar_url + ')',
+        backgroundImage: 'url(' + avatar_url + ')',
     };
     return (
         <div className="data-list">
             <div className="home-info">
                 <h1 className="home-info__title">
                     <div className="home-info__logo" style={companyImage}></div>
-                    <div className="home-info__title-text">{props.home.name} <div className="home-info__title-version">{props.home.bio}</div></div>
+                    <div className="home-info__title-text">{props.home.name} <div className="home-info__title-version">v 1.0 Beta</div></div>
                 </h1>
-                <div className="home-info__text">A module bundler for javascript application: BEM + Webpack + ES6 + HBS + SASS</div>
+                <div className="home-info__text">{props.home.description}</div>
                 <p><Link className="btn btn_lg btn_success" href={props.home.html_url}>Download</Link></p>
             </div>
         </div>
