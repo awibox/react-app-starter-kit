5d14890346e8dbc59ca980d7f199c9918a319cf8
Andrey Arkhipov <wibox@ya.ru>
2016-12-06 00:05:20 +0300

    author-api

---
+++ source/api/author-api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import store from '../store';
+import { getAuthorSuccess} from '../actions/author-actions';
+
+export function getAuthor() {
+    console.log('getAuthor сработал');
+    return axios.get('https://api.github.com/users/awibox')
+        .then(response => {
+            console.log("get",response);
+            store.dispatch(getAuthorSuccess(response.data));
+            return response;
+        });
+}
