86b6dcef95186ca734fe711e81b85eb775d86928
Andrey Arkhipov <wibox@ya.ru>
2016-12-06 00:06:24 +0300

    Add link in header for author

--- source/components/header/header.js
+++ source/components/header/header.js
@@ -13,6 +13,7 @@ const Header = React.createClass({
                     <aside className="header__menu">
                         <Link className="header__menu-item" to="/" activeClassName="header__menu-item_active">About</Link>
                         <Link className="header__menu-item" to="/repos" activeClassName="header__menu-item_active">Repositories</Link>
+                        <Link className="header__menu-item" to="/author" activeClassName="header__menu-item_active">Author</Link>
                     </aside>
                 </div>
             </header>
