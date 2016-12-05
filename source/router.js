04509abba29d9e322cdd6c1280d3ee6ba59028ca
Andrey Arkhipov <wibox@ya.ru>
2016-12-06 00:04:25 +0300

    Router for author

--- source/router.js
+++ source/router.js
@@ -7,12 +7,14 @@ import MainLayout from './layouts/main';
 // Pages
 import Home from './pages/home';
 import Repos from './pages/repos';
+import Author from './pages/author';
 
 export default (
     <Router history={browserHistory}>
         <Route component={MainLayout}>
             <Route path="/" component={Home} />
             <Route path="repos" component={Repos} />
+            <Route path="author" component={Author} />
         </Route>
     </Router>
 );
