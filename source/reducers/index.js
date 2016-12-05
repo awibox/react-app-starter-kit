f5c691853c5cfd4f61f14e077f07f2688138abea
Andrey Arkhipov <wibox@ya.ru>
2016-12-06 00:04:03 +0300

    Author reducers

--- source/reducers/index.js
+++ source/reducers/index.js
@@ -3,10 +3,12 @@ import { combineReducers } from 'redux';
 // Reducers
 import reposReducer from './repos-reducer';
 import homeReducer from './home-reducer';
+import authorReducer from './author-reducer';
 
 // Combine Reducers
 var reducers = combineReducers({
     homeState: homeReducer,
+    authorState: authorReducer,
     reposState: reposReducer,
 });
 
